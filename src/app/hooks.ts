f456162431496cf34e43938861b8dbf6aca7ede8
mhrynko <glukym@gmail.com>
2022-09-12 15:41:36 +0300

    after Simplify reducer and actions

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,4 +1,6 @@
+import { useDispatch } from 'react-redux';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import { RootState } from './store';
+import { AppDispatch, RootState } from './store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
--- src/features/position.ts
+++ src/features/position.ts
@@ -1,17 +1,4 @@
-type MoveLeftAction = { type: 'position/MOVE_LEFT' };
-type MoveRightAction = { type: 'position/MOVE_RIGHT' };
-type MoveUpAction = { type: 'position/MOVE_UP' };
-type MoveDownAction = { type: 'position/MOVE_DOWN' };
-
-type Action = MoveLeftAction
-  | MoveRightAction
-  | MoveUpAction
-  | MoveDownAction;
-
-const moveLeft = (): MoveLeftAction => ({ type: 'position/MOVE_LEFT' });
-const moveRight = (): MoveRightAction => ({ type: 'position/MOVE_RIGHT' });
-const moveUp = (): MoveUpAction => ({ type: 'position/MOVE_UP' });
-const moveDown = (): MoveDownAction => ({ type: 'position/MOVE_DOWN' });
+import { createSlice } from '@reduxjs/toolkit';
 
 type Position = {
   x: number;
@@ -20,24 +7,16 @@ type Position = {
 
 const startPosition: Position = { x: 0, y: 0 };
 
-const positionReducer = (
-  position: Position = startPosition,
-  action: Action,
-): Position => {
-  switch (action.type) {
-    case 'position/MOVE_LEFT':
-      return { ...position, x: position.x - 1 };
-    case 'position/MOVE_RIGHT':
-      return { ...position, x: position.x + 1 };
-    case 'position/MOVE_UP':
-      return { ...position, y: position.y - 1 };
-    case 'position/MOVE_DOWN':
-      return { ...position, y: position.y + 1 };
+const positionSlice = createSlice({
+  name: 'position',
+  initialState: startPosition,
+  reducers: {
+    moveLeft: position => ({ ...position, x: position.x - 1 }),
+    moveRight: position => ({ ...position, x: position.x + 1 }),
+    moveUp: position => ({ ...position, y: position.y - 1 }),
+    moveDown: position => ({ ...position, y: position.y + 1 }),
+  },
+});
 
-    default:
-      return position;
-  }
-};
-
-export default positionReducer;
-export const actions = { moveLeft, moveRight, moveUp, moveDown };
+export default positionSlice.reducer;
+export const { actions } = positionSlice;
