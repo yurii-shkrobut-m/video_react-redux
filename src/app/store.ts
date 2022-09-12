389d8793791afb8fb3fd368ff8fed1e9f86689d8
mhrynko <glukym@gmail.com>
2022-09-12 14:22:59 +0300

    after Redux Toolkit Intro

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,16 @@
-// npm i redux @types/redux
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
+
 import amountReducer from '../features/amount';
 import goodsReducer from '../features/goods';
 import positionReducer from '../features/position';
 
-const reducer = combineReducers({
-  amount: amountReducer,
-  goods: goodsReducer,
-  position: positionReducer,
+const store = configureStore({
+  reducer: {
+    amount: amountReducer,
+    goods: goodsReducer,
+    position: positionReducer,
+  },
 });
-
-const store = createStore(reducer, composeWithDevTools());
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
